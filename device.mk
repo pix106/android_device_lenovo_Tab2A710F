64c0f969e4c52c772cd731e3672a8662f5500762
Sébastien Bordenave <sbordenave@gmail.com>
2016-10-18 19:09:58 +0200

    Added a few permissions from stock rom, and a few commented one to be examined later

--- device.mk
+++ device.mk
@@ -65,6 +65,32 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
+    # permissions files that are in stock rom, and available in CM => added them 2016/10/08
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.software.webview.xml:system/etc/permissions/android.software.webview.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+    # Some permissions files that are in stock rom, but not in CM frameworks/native/data/etc/ => add them ? TODO
+    #system/etc/permissions/org.simalliance.openmobileapi.xml
+    #system/etc/permissions/com.android.media.remotedisplay.xml
+    #system/etc/permissions/android.software.live_wallpaper.xml
+    #system/etc/permissions/com.android.mediadrm.signer.xml
+    #system/etc/permissions/com.mediatek.effect.xml
+    #system/etc/permissions/com.google.android.maps.xml
+    #system/etc/permissions/com.android.location.provider.xml
+    #system/etc/permissions/com.google.widevine.software.drm.xml
+    #system/etc/permissions/com.google.android.media.effects.xml
+    #system/etc/permissions/platform.xml
+    #system/etc/permissions/android.mediatek.tedongle.xml
+
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
