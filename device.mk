2efbec4bdb960c0ee7d006380e08f75cc30b4a01
Sébastien Bordenave <sbordenave@gmail.com>
2016-10-30 14:13:29 +0100

    Replaced copied media_codec_google* with the exact same files from frameworks/av/media/libstagefright/data

--- device.mk
+++ device.mk
@@ -90,10 +90,10 @@ PRODUCT_COPY_FILES += \
 # media codecs files that are not copied from stock rom
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-	$(LOCAL_PATH)/configs/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-	$(LOCAL_PATH)/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Bluetooth config files
 PRODUCT_COPY_FILES += \
